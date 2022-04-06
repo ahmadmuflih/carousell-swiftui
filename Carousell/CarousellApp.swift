7e14ce919ef3cf9c96315d81a386610c054af6e4
Baso <baso.ahmad@ruangguru.com>
2022-04-06 21:01:39 +0800

    Initial Commit

---
+++ Carousell/CarousellApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarousellApp.swift
+//  Carousell
+//
+//  Created by Ruangguru on 06/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CarousellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
